c22d5021d08661fd7e83e7b76c96ec3c8359594c
Quinn Hart <qjhart@ucdavis.edu>
2017-07-26 12:43:10 -0700

    First Pass at county_codes.csv file.

--- utils/wikidata-to-csv.js
+++ utils/wikidata-to-csv.js
@@ -16,14 +16,14 @@ var get_countries = async function() {
     try {
 	let res = await request.get(url);
 	res = wdk.simplifySparqlResults(res.body);
-	for (var c=0; c<1; c++) {
-	    process.stdout.write(util.format('%d/%d\r',c,res.length));
+	for (var c=0; c<res.length; c++) {
+	    process.stderr.write(util.format('%d/%d\r',c,res.length));
 	    var i=res[c];
-	    var country = {code:i.code,country:i.country.label,alpha3:i.alpha3,flag:''};
+	    var country = {code:i.code,country:i.country.label,alpha3:i.alpha3,start:i.start,end:i.end,flag:''};
 	    if (i.flag) {
 		try {
 		    let svg = await request.get(i.flag).set('Accept', 'image/svg+xml');
-		    country.flag=svg.body;
+		    country.flag=svg.body.toString().replace(/\r?\n|\r/g,'').replace(/\s+/g,' ');
 		} catch (err) {
 		    console.log('Flag request failed:'+err);
 		}
@@ -38,7 +38,6 @@ var get_countries = async function() {
 
 get_countries()
     .then(country_list=>{
-	console.log(JSON.stringify(country_list));
 	jsonexport(country_list,(err,csv) => {
 	    if (err) { console.log(err) } ;
 	    console.log(csv);
@@ -47,25 +46,3 @@ get_countries()
 
 
 
-/*
- request.get(url)
-    .then( res => wdk.simplifySparqlResults(res.body))
-    .then((cc) => {
-	cc.forEach( i => {
-	    var country = {code:i.code,country:i.country.label,alpha3:i.alpha3,flag:null};
-	    if (i.flag) {
-		request.get(i.flag)
-		    .then(flag => {
-			country.flag=flag.res;
-			country_list.push(country);
-		    }).catch(err => {console.error(err); country_list.push(country)});
-	    } else {
-		country_list.push(country);
-	    }
-	})	
-	jsonexport(country_list,(err,csv) => {
-	    if (err) return console.error(err);
-	    console.log(csv);
-	});
-    }).catch(err => console.error(err))
-*/		      
